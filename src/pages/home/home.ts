f910e4fac0fc92ba143b69c079cd3774ea88ccec
Bruno Pimentel <brunoapimentel@gmail.com>
2018-01-29 18:41:26 -0200

    Adiciona página inicial e página lista

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ListaPage } from '../lista/lista';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage{
+  imagemVisivel = false;
 
   constructor(public navCtrl: NavController) {
+    setTimeout(this.exibirImagem.bind(this), 500);
+  }
 
+  exibirImagem(){
+    this.imagemVisivel = true;
+  }
+
+  irParaLista(){
+    this.navCtrl.push(ListaPage);
   }
 
 }
